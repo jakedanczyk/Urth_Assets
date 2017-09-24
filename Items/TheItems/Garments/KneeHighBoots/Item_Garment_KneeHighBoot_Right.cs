82b806306922a5549ce19c51fe3d1930f10018a9
jakedanczyk <danczykj@gmail.com>
2017-09-24 16:59:21 -0600

    a few new items and a bunch of line endings re-alligned

---
+++ Items/TheItems/Garments/KneeHighBoots/Item_Garment_KneeHighBoot_Right.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Garment_KneeHighBoot_Right : Item_Garment {
+
+	// Use this for initialization
+	void Start () {
+        bodyPartCoverage.Add(RPGStatType.RightFoot);
+    }
+
+}
